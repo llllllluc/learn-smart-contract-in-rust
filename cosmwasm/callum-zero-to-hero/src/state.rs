938fefdfa4a463635d8a90a828689423cf1c23aa
llllllluc <58892938+llllllluc@users.noreply.github.com>
2022-12-20 23:00:24 -0800

    finished first 7 lessons of cw 0-1, covered schema and init

---
+++ cosmwasm/callum-zero-to-hero/src/state.rs
@@ -0,0 +1,28 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+use cosmwasm_std::Addr;
+use cw_storage_plus::{Item, Map};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Config {
+    pub admin: Addr,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Poll {
+    pub creator: Addr,
+    pub question: String,
+    pub options: Vec<(String, u64)>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Ballot {
+    pub option: String,
+}
+
+pub const CONFIG: Item<Config> = Item::new("config");
+// key is poll id (uuid), val is actual poll
+pub const POOLS: Map<String, Poll> = Map::new("polls");
+// key is voter-poll_id pair, val is actual ballot (voting result)
+pub const BALLOTS: Map<(Addr, String), Ballot> = Map::new("ballots");
